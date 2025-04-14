07c382c635cb16d9ff215734d0e3f5585f73f3a3
iapam <70758592+iapam@users.noreply.github.com>
2025-04-14 10:31:36 +0000

    Update servicesRoute.js

--- backend/Routes/servicesRoute.js
+++ backend/Routes/servicesRoute.js
@@ -5,7 +5,7 @@ require('../Services/local-strategy')
 
 loginRouter.post("/agricom/auth/login",passport.authenticate('local'),(req,res)=>{
     const user=req.user;
-    hashPassword("hell")
+   // hashPassword("")
     res.status(200).send({msg:"login successfully"})
 })
 module.exports=loginRouter
