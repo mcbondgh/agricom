7da99e59a8b9e5055f9e1a30dcb3ee990461205c
mcbondgh <96586364+mcbondgh@users.noreply.github.com>
2025-05-06 01:02:39 +0000

    application logo changed

--- backend/Routes/EmployeeRouter.js
+++ backend/Routes/EmployeeRouter.js
@@ -1,30 +1,14 @@
 const express=require('express')
-<<<<<<< HEAD
-const {validateEmployeeDateInput,updateEmployeeMiddleWare,delete_employeeMiddleware}=require('../MiddleWares/EmployeeMiddleware')
-=======
 const validateEmployeeDateInput=require('../MiddleWares/EmployeeMiddleware')
->>>>>>> eb3524e93a46e0b4a04dd443f6651375b98d003a
 const EmployeeController=require('../Controllers/EmployeeController')
 const employeeRouter=express.Router()
 employeeRouter.post("/agricomfarms/agrocom/registeremployee",validateEmployeeDateInput,(req,res)=>{
     res.send({msg:`created`})
 })
-<<<<<<< HEAD
-employeeRouter.post("/agricomfarms/agrocom/update_employee/:employeeId",updateEmployeeMiddleWare,(req,res)=>{
-res.send({msg:"employee updated"})
-})
-=======
->>>>>>> eb3524e93a46e0b4a04dd443f6651375b98d003a
 const employeesobject=new EmployeeController()
 employeeRouter.get("/agricomfarms/agrocom/getallemployee",(req,res)=>{
 employeesobject.getAllEmployees().then((employee)=>{
 res.send(employee)
 })
 })
-<<<<<<< HEAD
-employeeRouter.get("/agricomfarms/agrocom/delete_employee/:employeeId",delete_employeeMiddleware,(req,res)=>{
-    res.send({msg:"employee deleted"})
-})
-=======
->>>>>>> eb3524e93a46e0b4a04dd443f6651375b98d003a
 module.exports=employeeRouter
--- backend/Routes/FarmerRouter.js
+++ backend/Routes/FarmerRouter.js
@@ -1,29 +1,7 @@
 const express=require('express')
-<<<<<<< HEAD
-const {updateMiddleware,validateFarmerInput,deleteFarmerMiddleware,validateFarmInfo,validateYieldInfo}=require('../MiddleWares/FarmerMiddleWare')
-const FarmerController=require('../Controllers/FarmerController')
-const farmerRouter=express.Router()
-farmerRouter.post("/agricomfarms/agrocom/registerfarmer",validateFarmerInput,validateFarmInfo,validateYieldInfo,(req,res)=>{
-    console.log({msg:"created"})
-    res.send({msg:`created`})
-})
-farmerRouter.post("/agricomfarms/agrocom/updatefarmers/:farmer_Id",updateMiddleware,(req,res)=>{ 
-    res.send({msg:"farmer updated"})
-})
-farmerRouter.get("/agricomfarms/agrocom/getallfarmers",(req,res)=>{
-    const farmerController=new FarmerController()
-    farmerController.getAllFarmers().then((allFarmers)=>{
-        res.send(allFarmers)
-    })
-    })
-farmerRouter.get("/agricomfarms/agrocom/deletefarmer/:farmerId",deleteFarmerMiddleware,(req,res)=>{ 
-    res.send({msg:"farmer deleted"})
-})
-=======
 const validateFarmerInput=require('../MiddleWares/FarmerMiddleWare')
 const farmerRouter=express.Router()
 farmerRouter.post("/agricomfarms/agrocom/registerfarmer",validateFarmerInput,(req,res)=>{
     res.send({msg:`created`})
 })
->>>>>>> eb3524e93a46e0b4a04dd443f6651375b98d003a
 module.exports=farmerRouter
